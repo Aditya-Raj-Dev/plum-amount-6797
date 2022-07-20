847e4a36351cc3deceeab82f1b481cdc7edc24c6
Aditya Raj <aadiyadav56837492@gmail.com>
2022-07-21 03:23:24 +0530

    home complete

--- beautybebo/src/Home/Newarrival.jsx
+++ beautybebo/src/Home/Newarrival.jsx
@@ -1,26 +1,67 @@
 import axios from "axios";
 import React, { Component, useContext } from 'react'
 import { Appcontext } from "../context/Appcontext";
+import styles from "../styles/home.module.css"
+import { Box ,Flex,Grid,Button} from '@chakra-ui/react'
+import { GiBeachBag} from 'react-icons/gi';
 
-import "react-alice-carousel/lib/alice-carousel.css";
-import AliceCarousel from 'react-alice-carousel';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "./styles.css";
+
+
+// import required modules
+import { FreeMode, Pagination } from "swiper";
 
 
 const Newarrival = () => {
    const {makeupdata}= useContext(Appcontext)
 
-   
-
-
-
-   console.log(makeupdata)
   return (
     <div>
         <div>
-            <img src="https://www.beautybebo.com/pub/media/blue_heaven.jpg"  />
+            <img src="https://www.beautybebo.com/pub/media/blue_heaven.jpg" style={{marginBottom:"2rem"}} />
         </div>
+       <div>
+       <h1>NEW ARRIVALS</h1>
+       <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        freeMode={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[FreeMode, Pagination]}
+        className="mySwiper"
+      >
        
-
+               {
+                
+               makeupdata.map((item)=>(
+                    <div className={styles.startproduct}>
+                      <SwiperSlide>
+                        <img src={item.img}/>
+                        <p>{item.name}</p>
+                        <h5>⭐⭐⭐⭐⭐</h5>
+                        <div >
+                            <s>₹ {item.cost}</s>
+                            <h3>₹ {item.price}</h3>
+                            <h4 >{item.off}</h4>
+                       </div>
+                       <Button>{<GiBeachBag/>} Add to cart</Button>
+                       </SwiperSlide>
+                    </div>
+                ))
+               }
+            
+      </Swiper>
+       </div>
+       
     </div>
   )
 }
--- beautybebo/src/Home/Sliding.jsx
+++ beautybebo/src/Home/Sliding.jsx
@@ -1,49 +1,91 @@
-import React, { useEffect, useState } from 'react'
-import { MdKeyboardArrowLeft } from "react-icons/md";
-import { MdKeyboardArrowRight } from "react-icons/md";
+// import React, { useEffect, useState } from 'react'
+// import { MdKeyboardArrowLeft } from "react-icons/md";
+// import { MdKeyboardArrowRight } from "react-icons/md";
 
-export default function Sliding() {
+// export default function Sliding() {
 
-    const [images, setImages] = useState([])
-    const [pointer, setPointer] = useState(0)
+//     const [images, setImages] = useState([])
+//     const [pointer, setPointer] = useState(0)
      
 
-    let image1="https://www.beautybebo.com/pub/media/mega-menu/slider-2.jpg";
-    let image2="https://www.beautybebo.com/pub/media/mega-menu/slider-3.jpg";
-    let image3="https://www.beautybebo.com/pub/media/mega-menu/slider-1.jpg";
+//     let image1="https://www.beautybebo.com/pub/media/mega-menu/slider-2.jpg";
+//     let image2="https://www.beautybebo.com/pub/media/mega-menu/slider-3.jpg";
+//     let image3="https://www.beautybebo.com/pub/media/mega-menu/slider-1.jpg";
 
   
-    useEffect(() => {
-      setImages([image1, image2, image3,])
+//     useEffect(() => {
+//       setImages([image1, image2, image3,])
   
-      let interval = setInterval(() => {
-        setPointer(pointer === images.length-1 ? 0 : (prev) => prev + 1);
-      }, 3000);
+//       let interval = setInterval(() => {
+//         setPointer(pointer === images.length-1 ? 0 : (prev) => prev + 1);
+//       }, 3000);
   
-      return () => clearInterval(interval);
-    }, [pointer])
+//       return () => clearInterval(interval);
+//     }, [pointer])
   
-    const handleForward = () => {
-      if ( pointer === 3 ) {
-        setPointer(0)
-      }
-      else {
-        setPointer(prev => prev + 1)
-      }
-    }
+//     const handleForward = () => {
+//       if ( pointer === 3 ) {
+//         setPointer(0)
+//       }
+//       else {
+//         setPointer(prev => prev + 1)
+//       }
+//     }
   
-    const handleBackword = () => {
-      if ( pointer === 0 ) {
-        setPointer(images.length-1)
-      }
-      else {
-        setPointer(prev => prev - 1)
-      }
-    }
+//     const handleBackword = () => {
+//       if ( pointer === 0 ) {
+//         setPointer(images.length-1)
+//       }
+//       else {
+//         setPointer(prev => prev - 1)
+//       }
+//     }
   
-    return (
-      <div >
-          <img src={images[pointer]} alt="" />
-      </div>
-    )
-  }
+//     return (
+//       <div >
+//           <img src={images[pointer]} alt="" />
+//       </div>
+//     )
+//   }
+
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+export default function Sliding() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={40}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://www.beautybebo.com/pub/media/mega-menu/slider-2.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://www.beautybebo.com/pub/media/mega-menu/slider-3.jpg" alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://www.beautybebo.com/pub/media/mega-menu/slider-1.jpg" alt="" />
+        </SwiperSlide>
+        
+      </Swiper>
+    </>
+  );
+}
